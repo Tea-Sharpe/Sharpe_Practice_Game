6b1084b39fcd12dc28aa61300359204b6d384272
Tea-Sharpe <48100190+Tea-Sharpe@users.noreply.github.com>
2019-03-29 16:53:58 -0400

    Add files via upload

---
+++ Enemy_Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public float movementSpeed = 1;
+    //int MaxDistance = 10;
+    //int MinDistance = 5;
+
+    void Start()
+    {
+        //GameObject.FindWithTag(Player);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(player.transform);
+        Vector3 localPosition = player.transform.position - transform.position;
+        transform.Translate(localPosition.x * Time.deltaTime * movementSpeed, localPosition.y * Time.deltaTime * movementSpeed, localPosition.z * Time.deltaTime * movementSpeed);
+        //transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
