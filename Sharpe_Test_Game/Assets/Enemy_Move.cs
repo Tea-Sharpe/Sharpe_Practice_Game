988a4ffb06c8f08f1778ff8e05051cefabb89458
Tea-Sharpe <48100190+Tea-Sharpe@users.noreply.github.com>
2019-04-04 18:41:25 -0400

    Updates to game
    What was added:
    -Enemy
    -Player
    -Enemy's ability to attack
    -Player's ability to attack

---
+++ Sharpe_Test_Game/Assets/Enemy_Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public float movementSpeed = 1;
+    public float MaxDistance = 10;
+    public float MinDistance = 5;
+    public float MaxAllowed = 3;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        movementSpeed = Random.Range(MinDistance, MaxDistance); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(player.transform.position, transform.position);
+        if (distance < MaxAllowed)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Sharpe_Test_Game/Assets/MovePicture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePicture : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Update()
+    {
+        //transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+
+
+        // Input.GetAxis("Vertical")  [-1, 1] Math
+        float axis_Y = transform.position.y + Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+
+        transform.position = new Vector2(transform.position.x, axis_Y);
+
+        //Vector3( X axis, Y axis, Z axis)
+
+        //EXample:
+        //transform.position  -> [output] (10, 17, 34);
+        //transform.position = new VEctor3( 3, transform.position.y, 0)
+        //transform.position  -> [output] (3, 17, 0);
+
+        //10k
+        //Time.deltaTime = [0,1] * 3 (speed)  = 1.5  30fps 60pfs 58=59  //to keep the speed constantly without matter the speed of diferent computers
+        if (Input.GetKey(KeyCode.A)) //10 -3 -> -2
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+
+        if (Input.GetKey("d")) //dont use Capital letter to dont crash the game
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+
+    }
+}
